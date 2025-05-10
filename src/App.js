27a3705573b67dda64fe61bf9a57fd7d7f82c1c6
ElMihalk <mikostrz@d17-429-pc-05.ipa.ii.agh.edu.pl>
2025-05-10 13:58:59 +0200

    Second savepoint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl')
+    const [emailStyleMessage, setEmailStyleMessage] = useState()
 
     function handleChange(event) {
-        console.log(event.target.value);
+        if (email.length < 10){
+            setEmailStyleMessage("Ale masz krótki adres");
+        } else if (email.length >= 10 & event.target.value.length < 20){
+            setEmailStyleMessage("Twój adres e-mail jest w sam raz!");
+        } else {
+            setEmailStyleMessage("Twój adres e-mail stanowczo za długi!");
+        }
+        setEmail(event.target.value);
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            <p>{emailStyleMessage}</p>
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
