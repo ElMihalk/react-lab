9f6cb7b0e604f3c4745c58a961738155a0adce85
ElMihalk <mikostrz@d17-429-pc-06.ipa.ii.agh.edu.pl>
2025-05-11 14:06:49 +0200

    Meeting list added

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,8 +1,13 @@
+import NewMeetingForm from "./meetings/NewMeetingForm";
+import MeetingsList from "./meetings/MeetingsList";
+import MeetingsPage from "./meetings/MeetingPage";
+
 export function UserPanel(props) {
     return (
         <div>
             <h2>Witaj {props.username}!</h2>
             <button onClick={() => props.logout()}>Wyloguj</button>
+            <MeetingsPage/>
         </div>
     )
 }
